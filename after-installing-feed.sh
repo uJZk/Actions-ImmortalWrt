4bb23650a4bbe575406ef235beefa3e223e59271
uJZk <34388941+uJZk@users.noreply.github.com>
2026-05-09 17:13:27 +0800

    modified:   .config 	modified:   .github/workflows/build-immortalwrt.yml 	modified:   after-installing-feed.sh 	modified:   before-installing-feed.sh 	new file:   config-6.12

--- after-installing-feed.sh
+++ after-installing-feed.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
@@ -18,3 +8,6 @@ sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
 # Partition alignment
 sed -i 's/256/4096/g' target/linux/x86/image/Makefile
+
+# Add kernel config
+mv ../config-6.12 target/linux/x86/config-6.12
--- before-installing-feed.sh
+++ before-installing-feed.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment feed sources
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Remove feed sources
 sed -i '/telephony/d' feeds.conf.default
+sed -i '/video/d' feeds.conf.default
 
 # Add feed sources
 echo 'src-git daed https://github.com/QiuSimons/luci-app-daed' >> feeds.conf.default
 echo 'src-git wrtbwmon https://github.com/brvphoenix/wrtbwmon' >> feeds.conf.default
 echo 'src-git wrtbwmon_luci https://github.com/brvphoenix/luci-app-wrtbwmon' >> feeds.conf.default
-# echo 'src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
